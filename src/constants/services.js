c3851f938909df2a6ea9975814ddd0b77ac16541
slavavisuals <slavavisuals@gmail.com>
2020-10-30 15:14:27 -0400

    changes in services

--- src/constants/services.js
+++ src/constants/services.js
@@ -1,24 +1,24 @@
 
 import React from "react"
 
-import { FaHtml5, FaJs, FaReact } from "react-icons/fa"
+import { FaLaptopCode, FaDrawPolygon, FaRegListAlt } from "react-icons/fa"
 export default [
   {
     id: 1,
-    icon: <FaHtml5 className="service-icon" />,
-    title: "HTML5",
-    text: `HTML5 is the latest evolution of the standard that defines HTML.`,
+    icon: <FaLaptopCode className="service-icon" />,
+    title: "Web Development",
+    text: `I design and develop websites and web applications from scratch and provide flexible services to meet needs of your project, whether it is landing page, enterprise site or online shop`,
   },
   {
     id: 2,
-    icon: <FaJs className="service-icon" />,
-    title: "JavaScript",
-    text: `JavaScript is a scripting or programming language that allows you to implement complex features on web pages`,
+    icon: <FaDrawPolygon className="service-icon" />,
+    title: "User Experience (UX)",
+    text: `I always ensure that the end user is at the heart of my design considerations, and offer a range of design services from UX research to interaction design, as well as full usability audit`,
   },
   {
     id: 3,
-    icon: <FaReact className="service-icon" />,
-    title: "React JS",
-    text: `React JS is an open-source, front end, JavaScript library for building user interfaces or UI components.`,
+    icon: <FaRegListAlt className="service-icon" />,
+    title: "Custom Applications",
+    text: `Whether it is React JS app or Angular, Svelte JS or Vue, I take your requirements and help you through a procedure to choose right technology and develop the app into a real working solution`,
   },
 ]
